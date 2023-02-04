5290f5f229f11bf9a563270f28976efd4c6b8a74
Manasvi070902 <alim.manasvi@gmail.com>
2023-02-04 17:55:03 +0530

    added CART Controller

--- src/main/java/com/oracle/training/repository/CartRepo.java
+++ src/main/java/com/oracle/training/repository/CartRepo.java
@@ -4,20 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.lang.Nullable;
 
 import com.oracle.training.model.Cart;
-<<<<<<< HEAD
-import com.oracle.training.model.Payment;
-
-
-public interface CartRepo extends CrudRepository<Cart,Integer>
-{
-	Cart findById(int id);
-}
-=======
 import com.oracle.training.model.User;
-
 
 public interface CartRepo extends CrudRepository<Cart,Integer>{
 	@Nullable
 	Cart findByUser(User user);
+	Cart findById(int id);
 }
->>>>>>> ae5fa43 (added CART Controller)
